688f59e7ed32b5fbdbf1d72963eb8750701b1b1d
Mario Bruno <mbrunostem@gmail.com>
2022-01-31 18:34:19 -0500

    submitting early will revisit

--- README.md
+++ README.md
@@ -13,6 +13,7 @@ TBD
 * Node
 * Express
 * Mongoose
+* Moment
 
 ## License
 The MIT License is a permissive free software license originating at the Massachusetts Institute of Technology (MIT) in the late 1980s. As a permissive license, it puts only very limited restriction on reuse and has, therefore, high license compatibility.
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,7 +1,67 @@
-const { Users } = require('../models');
+const { Users, Thoughts } = require('../models');
+const thoughtsController = require('./thoughtsController');
 
 const usersController = {
+    getAllUsers(req, res) {
+        Users.find()
+        .then(async(usersData) => {
+            res.json(usersData)
+        })
+        .catch((err) => {
+            console.log(err);
+            return res.status(500).json(err);
+          });
+    },
+    
+    getUserById(req, res) {
+        Users.findOne({
+            _id: req.params.userId
+        })
+        .populate("Friends")
+        .populate("Thoughts")
+        .then(async(usersData) => {
+            if (!usersData) {
+                return res.status(404).json({message: "User not found"})
+            }
+            res.json(usersData)
+        })
+        .catch((err) => {
+            console.log(err);
+            return res.status(500).json(err);
+          });
+    },
 
+    removeUser(req, res) {
+        Users.findOneAndDelete({
+            _id: req.params.userId
+        })
+        .then(async(usersData) => {
+            if (!usersData) {
+                return res.status(404).json({message: "User not found"})
+            }
+            return Thoughts.deleteMany({_id: {$in: usersData.Thoughts}})
+        })
+        .catch((err) => {
+            console.log(err);
+            return res.status(500).json(err);
+          });
+    },
+
+    updateUser(req, res) {
+        Users.findOneAndUpdate
+    },
+
+    addUser(req, res) {
+        Users.create
+    },
+
+    addFriend(req, res) {
+        Users.findOneAndUpdate
+    },
+    
+    removeFriend(req, res) {
+        Users.findOneAndUpdate
+    },
 };
 
 module.exports = usersController; 
